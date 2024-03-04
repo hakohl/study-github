9d8b19325d3b5d0cd47f7b9294c20253f5f606ad
Harald Kohlmann <Harald.Kohlmann@gmx.de>
2024-03-04 09:11:24 +0100

    add determining active workflows

---
+++ .github/scripts/filter_active_workflows.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# filter active workflows in a repository
+
+cd ./.github/scripts || exit 1
+
+active_workflows=()
+#export "${active_workflows[@]}"
+
+while read name state id; do
+    if [[ $name == "exercise-cli-github" ]]; then
+        continue
+    elif [[ $state == "active" ]]; then
+        active_workflows+=("$name")
+    else
+        continue
+    fi
+done < workflow_list.txt
+
+exit 0
