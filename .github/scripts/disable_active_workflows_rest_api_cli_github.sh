d2925241642e644775570aa585be82768d77e0e4
Harald Kohlmann <Harald.Kohlmann@gmx.de>
2024-03-07 10:32:38 +0100

    add files using curl

--- .github/scripts/disable_active_workflows_rest_api_cli_github.sh
+++ .github/scripts/disable_active_workflows_rest_api_cli_github.sh
@@ -6,7 +6,7 @@ response=$(gh api \
               -H "X-GitHub-Api-Version: 2022-11-28" \
               /repos/hakohl/study-github/actions/workflows)
   
-for workflow_id in $(echo "$response" | jq -cr '.workflows[] | select( .name != "exercise-cli-github" and .state == "active" ) | .id'); do
+for workflow_id in $(echo "$response" | jq -cr '.workflows[] | select( .name != "exercise-rest-api-cli-github" and .state == "active" ) | .id'); do
     gh api \
   --method PUT \
   -H "Accept: application/vnd.github+json" \
---
+++ .github/scripts/disable_active_workflows_rest_api_curl_github.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# get active workflows in a repository, using curl for accessing REST API
+
+response=$(curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer 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" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/hakohl/study-github/actions/workflows)
+  
+for workflow_id in $(echo "$response" | jq -cr '.workflows[] | select( .name != "exercise-rest-api-curl-github" and .state == "active" ) | .id'); do
+  curl -L \
+    -X PUT \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer 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" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/repos/hakohl/study-github/actions/workflows/"${workflow_id}"/disable
+done
+
+exit 0
